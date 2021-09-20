23b48452139bbdc773ef607ba6155f7e2955aa1a
grcbarros <33353638+grcbarros@users.noreply.github.com>
2021-09-20 21:50:34 +0100

    Implementing possible moves of King

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -1,6 +1,7 @@
 package pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,28 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	private boolean canMove(Position position){
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; 
+
+		Position p = new Position(0, 0);
+
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (!(i == 0 && j == 0)) {
+					p.setValues(position.getRow() + i, position.getColumn() + j);
+					if (getBoard().positionExists(p) && canMove(p)) {
+						mat[p.getRow()][p.getColumn()] = true;
+					}
+				}
+			}
+		}
+
 		return mat;
 	}
 
